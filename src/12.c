1ed057e9deb0988b53bddc1aa55d7bfdf6d17274
daxanett306 <daxanett306@gmail.com>
2025-03-18 05:27:18 +0000

    Add 12.c

---
+++ src/12.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 5;
+    int num2 = 10;
+    if (num1 > num2) {
+        printf("num1 is greater than num2\n");
+    } else {
+        printf("num2 is greater than num1\n");
+    }
+    return 0;
+}
